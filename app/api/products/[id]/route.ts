8ddbd7c3653cc8aae93f4ceae95a87ea52ae8f5e
Chisom9ty <147661596+Chisom9ty@users.noreply.github.com>
2024-10-23 06:05:06 +0100

    Add files via upload

---
+++ app/api/products/[id]/route.ts
@@ -0,0 +1,54 @@
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "@/utils/connect";
+
+
+
+export const GET = async (
+    req:NextRequest,
+    {params}:{params:{id:string}})=>{
+    const { id } = params;
+    try{
+
+
+        const product = await prisma.product.findUnique({
+            where:{
+                id:id
+            },
+            
+        });
+        return new NextResponse (JSON.stringify(product),{status:200});
+    
+
+    }catch(err){
+        console.log(err);
+        return new NextResponse (JSON.stringify({message:"Something went wrrong!"}),{status:500})
+    }
+
+}
+
+export const DELETE = async (
+    req:NextRequest,
+    {params}:{params:{id:string}})=>{
+    const { id } = params;
+    const session = await getAuthSession()
+    if(session?.user.isAdmin){
+    try{
+
+
+         await prisma.product.delete({
+            where:{
+                id:id
+            },
+            
+        });
+        return new NextResponse (JSON.stringify("Product has been deleted!"),{status:200});
+    
+
+    }catch(err){
+        console.log(err);
+        return new NextResponse (JSON.stringify({message:"Something went wrrong!"}),{status:500})
+    }
+
+}
+return new NextResponse (JSON.stringify({message:"You are not allowed!"}),{status:403})
+}
